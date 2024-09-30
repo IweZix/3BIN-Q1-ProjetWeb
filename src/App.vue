9de69da5d010de044f53f56794b6043c078219b0
LucaN <lucanclv2@gmail.com>
2024-09-30 15:44:16 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+/**
+ * Import of App component
+ */
+import NavbarComponent from '@/layouts/NavbarComponent.vue';
+</script>
+
+<template>
+  <NavbarComponent />
+  <router-view />
+</template>
+
+<style scoped>
+</style>
