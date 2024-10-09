b718a4e1dbf34cc770dc7ffc94e3bf46dce3cc5f
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-10-09 14:39:23 +0200

    add LogoutComponent -> no need a Page

---
+++ src/components/Auth/LogoutComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { logout } from '@/services/auths';
+
+export default {
+  name: 'LogoutComponent',
+  async mounted() {
+    try {
+
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+
+        
+      await logout();
+      this.$router.push({ name: 'Login' });
+    } catch (error) {
+      console.error('An error occurred while logging out:', error);
+    }
+  },
+  render() {
+    return null; // Ce composant n'a pas besoin de template
+  }
+};
+</script>
