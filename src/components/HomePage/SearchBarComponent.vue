5e6daf5538dd978e4d6a971e53b1360f8e93f9a7
LotfiM1998 <113674279+LotfiM1998@users.noreply.github.com>
2024-10-15 15:28:37 +0200

    add: component hompage

--- src/components/HomePage/SearchBarComponent.vue
+++ src/components/HomePage/SearchBarComponent.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import router from '@/router/router';
 import { searchHomepage } from '@/services/searchHomepage';
+import { searchHomepage } from '@/services/searchHomepage';
 
 export default {
   /**
@@ -50,7 +51,65 @@ export default {
       <button class="submit-button" type="submit">Submit</button>
     </form>
   </div>
+  <div class="form-container">
+    <form @submit.prevent="submitForm">
+      <div>
+        <input type="text" id="name" placeholder="Enter the name of the song or the artist:" v-model="formData.name" />
+      </div>
+      <div>
+        <span v-if="errors.name" class="error ">{{ errors.name }}</span>
+      </div>
+      <button class="submit-button" type="submit">Submit</button>
+    </form>
+  </div>
 </template>
+
+
+<style scoped>
+#name {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #000000;
+  border-radius: 4px;
+  box-sizing: content-box;
+}
+
+.form-container {
+  width: 300px;
+  margin: 0 auto;
+}
+
+.error {
+  color: red;
+  font-size: 12px;
+}
+
+.submit-button {
+  width: 100%;
+  padding: 10px;
+  background-color: #319f35;
+  color: rgb(0, 0, 0);
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.submit-button:hover {
+  background-color: #45a049;
+}
+
+.submit-button:active {
+  background-color: #388e3c;
+}
+
+.submit-button:disabled {
+  background-color: #ddd;
+  cursor: not-allowed;
+}
+</style>
 
 
 <style scoped>
