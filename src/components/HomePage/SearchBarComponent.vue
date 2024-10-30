b8ea3e70c46229b4cc4367915f26e1f244c97ea3
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-10-30 23:49:53 +0100

    SearchBarComponent ok

--- src/components/HomePage/SearchBarComponent.vue
+++ src/components/HomePage/SearchBarComponent.vue
@@ -37,29 +37,25 @@ export default {
         const musicFound = await searchHomepage(this.formData.name);
         console.log(musicFound);
         this.$emit('update:listMusic', musicFound);
-        
-
       } 
     }
   }
 };
 </script>
 
-
 <template>
   <div class="form-container">
     <form @submit.prevent="submitForm">
       <div>
-        <input type="text" id="name" placeholder="Enter the name of the song or the artist:" v-model="formData.name" />
+        <input type="text" id="name" placeholder="Enter the name of the song or the artist:" v-model="formData.name" class="custom-input" />
       </div>
       <div>
-        <span v-if="errors.name" class="error ">{{ errors.name }}</span>
+        <span v-if="errors.name" class="error">{{ errors.name }}</span>
       </div>
       <button class="submit-button" type="submit">Submit</button>
     </form>
   </div>
 </template>
-
 
 <style scoped>
 #name {
@@ -105,6 +101,26 @@ export default {
   background-color: #ddd;
   cursor: not-allowed;
 }
+
+.custom-input {
+  width: 100%; 
+  margin: 10px 0; 
+  padding: 10px 15px; 
+  border: 1px solid #ddd; 
+  border-radius: 6px; 
+  background-color: #262727; 
+  font-size: 16px; 
+  color: #6c757d; 
+  outline: none; 
+  transition: border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out; 
+}
+
+.custom-input::placeholder {
+  color: white;
+}
+
+.custom-input:focus {
+  border-color: #80bdff; 
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25); 
+}
 </style>
-
-
